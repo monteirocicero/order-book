7fa8fc8a25af7ef2e6625e9063d95d6f53d1b5d1
Cicero Monteiro <cicerolmonteiro@gmail.com>
2022-05-18 22:25:52 -0300

    Refactoring docker configurations

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
-FROM ghcr.io/graalvm/graalvm-ce:20.3.6
-COPY build/libs/order-book-0.0.1-SNAPSHOT.jar order-book-0.0.1.jar
-ENTRYPOINT ["java","-jar","/order-book-0.0.1.jar"]
+FROM openjdk:17.0.2-slim-buster
+VOLUME /main-app
+ADD build/libs/order-book-0.0.1-SNAPSHOT.jar app.jar
+ENTRYPOINT ["java", "-jar","/app.jar"]
--- src/test/java/com/orecic/orderbook/OrderBookApplicationTests.java
+++ src/test/java/com/orecic/orderbook/OrderBookApplicationTests.java
@@ -12,8 +12,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -22,10 +23,10 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
-
-
-@SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class OrderBookApplicationTests {
 
     @Autowired
