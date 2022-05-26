120c97d2255b11d30e36f9a486613ed7618ab924
Cicero Monteiro <cicerolmonteiro@gmail.com>
2022-05-26 20:37:59 -0300

    Adding micrometer with new relic

---
+++ src/main/java/com/orecic/orderbook/infraestructure/config/newrelic/MicrometerConfig.java
@@ -0,0 +1,72 @@
+package com.orecic.orderbook.infraestructure.config.newrelic;
+
+
+import com.newrelic.telemetry.Attributes;
+import com.newrelic.telemetry.micrometer.NewRelicRegistry;
+import com.newrelic.telemetry.micrometer.NewRelicRegistryConfig;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.util.NamedThreadFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.Duration;
+
+@Configuration
+@AutoConfigureBefore({
+        CompositeMeterRegistryAutoConfiguration.class,
+        SimpleMetricsExportAutoConfiguration.class
+})
+@AutoConfigureAfter(MetricsAutoConfiguration.class)
+@ConditionalOnClass(NewRelicRegistry.class)
+public class MicrometerConfig {
+
+    @Bean
+    public NewRelicRegistryConfig newRelicConfig() {
+        return new NewRelicRegistryConfig() {
+            @Override
+            public String get(String key) {
+                return null;
+            }
+
+            @Override
+            public String apiKey() {
+                return "ace9dc20520a1099ed7647fee6d0de48a72dNRAL";
+            }
+
+            @Override
+            public Duration step() {
+                return Duration.ofSeconds(5);
+            }
+
+            @Override
+            public String serviceName() {
+                return "Order Book";
+            }
+
+        };
+    }
+
+    @Bean
+    public NewRelicRegistry newRelicMeterRegistry(NewRelicRegistryConfig config)
+            throws UnknownHostException {
+        NewRelicRegistry newRelicRegistry =
+                NewRelicRegistry.builder(config)
+                        .commonAttributes(
+                                new Attributes()
+                                        .put("host", InetAddress.getLocalHost().getHostName()))
+                        .build();
+        newRelicRegistry.config().meterFilter(MeterFilter.ignoreTags("plz_ignore_me"));
+        newRelicRegistry.config().meterFilter(MeterFilter.denyNameStartsWith("jvm.threads"));
+        newRelicRegistry.start(new NamedThreadFactory("newrelic.micrometer.registry"));
+        return newRelicRegistry;
+    }
+}
